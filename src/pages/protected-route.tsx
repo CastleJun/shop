72e2ba4c40c4a7931067af0362c331be04167d17
dev.jun28 <hsj9360@gmail.com>
2022-11-22 04:55:38 +0900

    feat: add protected route effect

---
+++ src/pages/protected-route.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+
+import { useAuthContext } from '../components/context/auth-context';
+
+interface Props {
+  requireAdmin?: boolean;
+  children: React.ReactElement;
+}
+
+const ProtectedRoute: React.FC<Props> = (props) => {
+  const { requireAdmin, children } = props;
+  const { user } = useAuthContext();
+
+  if (!user || (requireAdmin && !user.isAdmin)) {
+    return <Navigate to="/" replace />;
+  }
+
+  return children;
+};
+
+export default ProtectedRoute;
