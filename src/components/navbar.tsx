2c8c6ff233944182cef96a8719bbc2571445281e
dev.jun28 <hsj9360@gmail.com>
2022-11-22 04:56:02 +0900

    feat: add navbar component

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,22 +3,32 @@ import { BsFillPencilFill } from 'react-icons/bs';
 import { FiShoppingBag } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
 
+import Button from './base-component/button';
+import { useAuthContext } from './context/auth-context';
+import User from './user';
+
 interface Props {}
 
-const Navbar: React.FC<Props> = (props) => {
+const Navbar: React.FC<Props> = () => {
+  const { user, login, logout } = useAuthContext();
+
   return (
-    <header>
-      <Link to="/">
+    <header className="flex justify-between border-b border-gray-300 p-2">
+      <Link to="/" className="flex items-center text-4xl text-brand">
         <FiShoppingBag />
         <h1>Shop</h1>
       </Link>
-      <nav>
+      <nav className="flex items-center gap-4 font-semibold">
         <Link to="/products">Products</Link>
-        <Link to="carts">Carts</Link>
-        <Link to="products/new">
-          <BsFillPencilFill />
-        </Link>
-        <button>Login</button>
+        {user && <Link to="carts">Carts</Link>}
+        {user && user?.isAdmin && (
+          <Link to="products/new" className="text-2xl">
+            <BsFillPencilFill />
+          </Link>
+        )}
+        {!!user && <User user={user} />}
+        {!user && <Button onClick={login} text="Login" />}
+        {!!user && <Button onClick={logout} text="Logout" />}
       </nav>
     </header>
   );
