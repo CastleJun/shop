6dae194aaafdc0b5d8827a6f4b1144afb1fa7044
dev.jun28 <hsj9360@gmail.com>
2022-11-22 04:55:11 +0900

    feat: add user context api

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,15 @@ import './App.css';
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 
+import { AuthContextProvider } from './components/context/auth-context';
 import Navbar from './components/navbar';
 
 const App = () => {
   return (
-    <>
+    <AuthContextProvider>
       <Navbar />
       <Outlet />
-    </>
+    </AuthContextProvider>
   );
 };
 
