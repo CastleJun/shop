60426b15f6408bf7601de639d5c8ffd0455059fc
dev.jun28 <hsj9360@gmail.com>
2022-11-23 05:49:48 +0900

    feat: add upload image function

---
+++ src/api/uploader.ts
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+export const uploadImage = async (file: Blob) => {
+  console.log(file);
+  const formData = new FormData();
+  formData.append('file', file);
+  formData.append('upload_preset', process.env.REACT_APP_CLOUDINARY_PRESET as string);
+
+  const { data } = await axios.post(process.env.REACT_APP_CLOUDINARY_URL as string, formData);
+
+  return data;
+};
