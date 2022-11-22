685e864295ed5d71b11c1e48a123ec58e82ac5e2
dev.jun28 <hsj9360@gmail.com>
2022-11-23 05:50:20 +0900

    refactor: modify button component

--- src/components/base-component/button.tsx
+++ src/components/base-component/button.tsx
@@ -2,13 +2,21 @@ import React from 'react';
 
 interface Props {
   text: string;
-  onClick: () => void;
+  onClick?: () => void;
+  type?: React.ButtonHTMLAttributes<HTMLButtonElement>['type'];
+  disabled?: boolean;
 }
 
 const Button: React.FC<Props> = (props) => {
-  const { text, onClick } = props;
+  const { text, onClick, type = 'button', disabled } = props;
   return (
-    <button className="bg-brand py-2 px-4 text-white rounded-sm hover:brightness-110" type="button" onClick={onClick}>
+    <button
+      // eslint-disable-next-line react/button-has-type
+      type={type}
+      disabled={disabled}
+      className="bg-brand py-2 px-4 text-white rounded-sm hover:brightness-110"
+      onClick={onClick}
+    >
       {text}
     </button>
   );
