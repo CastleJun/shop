ebf39c93f4374b6632926387b80686a486688f3d
dev.jun28 <hsj9360@gmail.com>
2022-11-24 06:08:12 +0900

    feat: add product card component

---
+++ src/components/product-card.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { Products } from '../api/firebase';
+
+interface Props {
+  product: Products;
+}
+
+const ProductCard: React.FC<Props> = (props) => {
+  const { product } = props;
+  const { id, price, image, title, category } = product;
+
+  const navigate = useNavigate();
+
+  return (
+    // eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-noninteractive-element-interactions
+    <li
+      onClick={() =>
+        navigate(`/products/${id}`, {
+          state: { product },
+        })
+      }
+      className="rounded-lg shadow-md overflow-hidden cursor-pointer transition-all hover:scale-105"
+    >
+      <img className="w-full" src={image} alt={title} />
+      <div className="mt-2 px-2 text-lg flex justify-between items-center">
+        <h3 className="truncate">{title}</h3>
+        <p>{`₩${price.toLocaleString()}`}</p>
+      </div>
+      <p className="mb-2 px-2 text-gray-600">{category}</p>
+    </li>
+  );
+};
+
+export default ProductCard;
