ba8b59c3580b458f5a7d3599f31c1bb8006ecfa3
dev.jun28 <hsj9360@gmail.com>
2022-11-24 06:07:50 +0900

    refactor: modify interface button

--- src/components/base-component/button.tsx
+++ src/components/base-component/button.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 interface Props {
   text: string;
-  onClick?: () => void;
+  onClick?: React.MouseEventHandler<HTMLElement>;
   type?: React.ButtonHTMLAttributes<HTMLButtonElement>['type'];
   disabled?: boolean;
 }
