7a4b501070031dfd51f2cee829f7f80456d3e48f
dev.jun28 <hsj9360@gmail.com>
2022-11-24 06:12:13 +0900

    feat: add all product

---
+++ src/pages/all-product.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Products from '../components/products';
+
+interface Props {}
+
+const AllProduct: React.FC<Props> = (props) => {
+  return <Products />;
+};
+
+export default AllProduct;
