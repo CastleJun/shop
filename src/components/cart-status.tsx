360f43e9559d71066c384adbfe89e84b34f4fc2f
dev.jun28 <hsj9360@gmail.com>
2022-11-24 20:32:34 +0900

    feat: add cart status component

---
+++ src/components/cart-status.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { AiOutlineShoppingCart } from 'react-icons/ai';
+
+import { useCart } from '../hooks/useCart';
+
+interface Props {}
+
+const CartStatus: React.FC<Props> = () => {
+  const {
+    cartQuery: { data: products },
+  } = useCart();
+
+  return (
+    <div className="relative">
+      <AiOutlineShoppingCart className="text-4xl" />
+      {products && (
+        <p className="w-6 h-6 text-center bg-brand text-white font-bold rounded-full absolute -top-1 -right-2">
+          {products.length}
+        </p>
+      )}
+    </div>
+  );
+};
+
+export default CartStatus;
