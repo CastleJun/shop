8e52fd77bfb390cd9927bce27fbf9020dd03f611
dev.jun28 <hsj9360@gmail.com>
2022-11-24 20:35:36 +0900

    refactor: modify useQuery to custom hook

--- src/components/products.tsx
+++ src/components/products.tsx
@@ -1,14 +1,15 @@
-import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 
-import { getProducts } from '../api/firebase';
+import { useProducts } from '../hooks/useProducts';
 import { isError } from '../util';
 import ProductCard from './product-card';
 
 interface Props {}
 
 const Products: React.FC<Props> = () => {
-  const { isLoading, error, data: products } = useQuery(['products'], getProducts);
+  const {
+    productsQuery: { isLoading, error, data: products },
+  } = useProducts();
 
   return (
     <>
