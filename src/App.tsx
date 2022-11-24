e0c33a3d5baef358dab00f6bb24439a49f556e57
dev.jun28 <hsj9360@gmail.com>
2022-11-24 20:36:54 +0900

    refactor: change directory

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 
-import { AuthContextProvider } from './components/context/auth-context';
 import Navbar from './components/navbar';
+import { AuthContextProvider } from './context/auth-context';
 
 const App = () => {
   const queryClient = new QueryClient();
